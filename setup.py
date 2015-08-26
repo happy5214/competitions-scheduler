3709bb1bfef11351ab3801d3e2fd49db52b6323c
Alexander Jones <happy5214@gmail.com>
2015-08-26 03:13:44 -0500

    Bumping version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1a1',
+    version='0.1b1',
 
     description='Generic schedulers for competitions',
 #    long_description=long_description,
@@ -36,7 +36,7 @@ setup(
     author_email='happy5214@gmail.com',
 
     # Choose your license
-    license='GPLv3',
+    license='GPLv3orlater',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -54,12 +54,9 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
 
     # What does your project relate to?
